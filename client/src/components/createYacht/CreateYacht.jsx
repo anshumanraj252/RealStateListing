6e1b7bf7af049c3aba13cebb445713f7e6e13b16
Anshuman <anshumanraj252@gmail.com>
2023-09-20 17:47:33 +0530

    REMOVE UNNECCESSARY IMPORT

--- client/src/components/createYacht/CreateYacht.jsx
+++ client/src/components/createYacht/CreateYacht.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react'
-import { AiFillFileImage } from 'react-icons/ai'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { request } from '../../util/fetchAPI'
